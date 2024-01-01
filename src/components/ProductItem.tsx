07bcade956e14fb3946fe06f4dd7619b35cdfe2e
Jhenbert Villamucho <jvv205756@gmail.com>
2024-01-01 17:22:47 +0800

    added type narrowing

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -29,40 +29,41 @@ const ProductItem = () => {
 
   if (loading) return <div>Loading...</div>;
 
-  return (
-    <>
-      <div className="single__product">
-        <h1>
-          <span onClick={() => navigate("/")}>🔙</span>
-          <span>
-            {singleProduct?.itemName} Page ID: {id}
-          </span>
-        </h1>
-        <section>
-          <figure className="product__img-container">
-            <img
-              className="product__img"
-              src={singleProduct?.imageUrl}
-              alt="image"
-            />
-          </figure>
-          <aside>
-            <h2>{singleProduct?.itemName}</h2>
-            <h3>{singleProduct?.notes}</h3>
-            <h4>
-              Category: <span>{singleProduct?.type}</span>
-            </h4>
-            <p>
-              Width: <strong>{singleProduct?.size?.width}</strong>
-            </p>
-            <p>
-              Length: <strong>{singleProduct?.size?.length}</strong>
-            </p>
-          </aside>
-        </section>
-      </div>
-    </>
-  );
+  if (singleProduct)
+    return (
+      <>
+        <div className="single__product">
+          <h1>
+            <span onClick={() => navigate("/")}>🔙</span>
+            <span>
+              {singleProduct.itemName} Page ID: {id}
+            </span>
+          </h1>
+          <section>
+            <figure className="product__img-container">
+              <img
+                className="product__img"
+                src={singleProduct.imageUrl}
+                alt="image"
+              />
+            </figure>
+            <aside>
+              <h2>{singleProduct.itemName}</h2>
+              <h3>{singleProduct.notes}</h3>
+              <h4>
+                Category: <span>{singleProduct.type}</span>
+              </h4>
+              <p>
+                Width: <strong>{singleProduct.size.width}</strong>
+              </p>
+              <p>
+                Length: <strong>{singleProduct.size.length}</strong>
+              </p>
+            </aside>
+          </section>
+        </div>
+      </>
+    );
 };
 
 export default ProductItem;
